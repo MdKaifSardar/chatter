d6181fe96d5e7e31100f60a1d68dfa46aade6971
Md Kaif Sardar <mkaifsard564773@gmail.com>
2025-04-15 01:34:23 +0530

    first commit

---
+++ src/app/api/signal/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+import Pusher from "pusher";
+
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID!,
+  key: process.env.PUSHER_KEY!,
+  secret: process.env.PUSHER_SECRET!,
+  cluster: process.env.PUSHER_CLUSTER!,
+  useTLS: true,
+});
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const { type, ...data } = body;
+
+  await pusher.trigger("video-chat", type, data);
+  return NextResponse.json({ message: "ok" }, { status: 200 });
+}
